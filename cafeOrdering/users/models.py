d2f68dc7469d99a540a7d03317c3b362a10940fe
Felix Partington <felix@Felixs-Mac-mini.local>
2023-08-01 17:07:18 +0100

    fixed user profile creating upon sign up, changed some styling to django accounts templates

--- cafeOrdering/users/models.py
+++ cafeOrdering/users/models.py
@@ -1,18 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+    # Add other fields for the user profile as needed
 
-# Signal to create a user profile when a new user is registered
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    def __str__(self):
+        return self.user.username
