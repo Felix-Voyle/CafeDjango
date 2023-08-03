0c46abbd518a8067df93c3559c0692598f5037c8
Felix Partington <felix@Felixs-Mac-mini.local>
2023-08-03 21:27:03 +0100

    added rough enquiry page and enquiry form submission logic

--- cafeOrdering/enquire/views.py
+++ cafeOrdering/enquire/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EnquiryForm
 
 def enquire(request):
-    return render(request, 'enquire/enquire.html')
+    if request.method == 'POST':
+        form = EnquiryForm(request.POST)
+        if form.is_valid():
+            
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            
+            form.save()
+
+            return redirect('/')
+    else:
+        form = EnquiryForm()
+    return render(request, 'enquire/enquire.html', {'form': form})
--- cafeOrdering/users/admin.py
+++ cafeOrdering/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from django.contrib import admin
 from django.contrib.sites.models import Site
 
 
