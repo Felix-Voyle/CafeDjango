a358115d0b551a586d0c6f8302f86c2e01f0d1ca
Felix Partington <felix@Felixs-Mac-mini.local>
2024-02-28 18:15:01 +0000

    added to userProfile model and updated admin.py

--- cafeOrdering/users/admin.py
+++ cafeOrdering/users/admin.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialAccount, SocialToken, SocialApp
 
+from .models import UserProfile
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'is_staff', 'is_workspace', 'address_line1', 'postcode']
+    list_filter = ['is_staff', 'is_workspace']
+    search_fields = ['user__username', 'address_line1', 'postcode']
+
+admin.site.register(UserProfile, UserProfileAdmin)
 
 # Unregister the Group model from the admin site
 admin.site.unregister(Group)
--- cafeOrdering/users/models.py
+++ cafeOrdering/users/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add other fields for the user profile as needed
+    is_staff = models.BooleanField('Staff', default=False)
+    is_workspace = models.BooleanField('Workspace', default=False)
+    address_line1 = models.CharField('Address Line 1', max_length=100)
+    address_line2 = models.CharField('Address Line 2', max_length=100, blank=True, null=True)
+    address_line3 = models.CharField('Address Line 3', max_length=100, blank=True, null=True)
+    postcode = models.CharField('Postcode', max_length=10)
 
     def __str__(self):
         return self.user.username
