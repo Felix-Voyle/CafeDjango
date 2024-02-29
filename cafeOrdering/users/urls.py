e2e7b14f7275548f0c0b921841f3aa3abcb56fa0
Felix <felix@Felixs-MacBook-Pro.local>
2024-02-29 21:50:21 +0000

    added functionality to report problem with order

--- cafeOrdering/users/urls.py
+++ cafeOrdering/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import view_profile
+from .views import sign_up, view_profile, report_problem
 
 urlpatterns = [
-    
+    path('profile/', view_profile, name='view_profile'),
+    path('sign_up/', sign_up, name='sign_up'),
+    path('report_problem/', report_problem, name='report_problem'),
 ]
